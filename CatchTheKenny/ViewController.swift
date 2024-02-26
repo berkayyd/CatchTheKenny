55cd6459e17378cdb7bcb84cf180784b0895339a
Berkay Demir <berkayydmir@gmail.com>
2024-02-26 14:39:24 +0300

    Initial Commit

---
+++ CatchTheKenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheKenny
+//
+//  Created by Berkay Demir on 26.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
